7603dafa1949b31e7f118bd3acb206bc62960667
zaehorang <zaehorang2@naver.com>
2024-10-03 16:41:19 +0900

    chore: #1 Update project

---
+++ FiveGuyes/FiveGuyes/FiveGuyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiveGuyesApp.swift
+//  FiveGuyes
+//
+//  Created by zaehorang on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct FiveGuyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
