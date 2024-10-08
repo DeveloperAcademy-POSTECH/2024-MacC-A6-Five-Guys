dd4dbec83ef0addaf0aab8223027b5cd00ef692a
hyeyeonie <shy020916@naver.com>
2024-10-08 19:09:08 +0900

    Feat: 사진의 메타데이터(위치, 시간) 불러오기 - 이미지 띄우기 구현 - 메타데이터 불러오기 일부 구현

---
+++ FiveGuyes/FiveGuyes/Sources/Views/ImageMetadata.swift
@@ -0,0 +1,16 @@
+//
+//  ImageMetadata.swift
+//  FiveGuyes
+//
+//  Created by 신혜연 on 10/7/24.
+//
+
+// import CoreLocation
+// import Foundation
+// import PhotosUI
+//
+// struct ImageMetadata {
+//    var image: UIImage
+//    var creationDate: Date?
+//    var location: CLLocation?
+// }
