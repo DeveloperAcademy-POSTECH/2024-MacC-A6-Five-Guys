ca4e99096f5a6866622be33552ec8e236936d9be
hyeyeonie <shy020916@naver.com>
2024-10-10 21:35:34 +0900

    Feat: TestingMetaView 구현 - PHAsset으로 변경해 HEIF형식의 이미지의 메타데이터 불러오기 구현 - 린트 반영해 코드 수정

--- FiveGuyes/FiveGuyes/Sources/Views/ImageMetadata.swift
+++ FiveGuyes/FiveGuyes/Sources/Views/ImageMetadata.swift
@@ -5,12 +5,11 @@
 //  Created by 신혜연 on 10/7/24.
 //
 
-// import CoreLocation
-// import Foundation
-// import PhotosUI
-//
-// struct ImageMetadata {
-//    var image: UIImage
-//    var creationDate: Date?
-//    var location: CLLocation?
-// }
+import CoreLocation
+import Foundation
+import PhotosUI
+
+struct ImageMetadata {
+    var imageDate: Date?
+    var location: String?
+}
