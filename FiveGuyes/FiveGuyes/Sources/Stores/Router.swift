69da8b3bf30a71cdc9b6c2f7d917a22dfc6849e9
Shim Hyeonhee <shimhyeonhee@10-141-55-188.0200.pos.kr.iosda.org>
2024-10-14 17:34:32 +0900

    feat:#23-chatView-fixing conflict

---
+++ FiveGuyes/FiveGuyes/Sources/Stores/Router.swift
@@ -0,0 +1,35 @@
+//
+//  Router.swift
+//  FiveGuyes
+//
+//  Created by Shim Hyeonhee on 10/13/24.
+//
+
+import SwiftUI
+
+enum ChatViewRoute: Hashable {
+    case photo
+    case emotion
+    case complete
+}
+
+class NavigationRouter<Path: Hashable>: ObservableObject {
+
+    @Published public var paths: [Path] = []
+
+    func push(_ path: Path) {
+        paths.append(path)
+    }
+
+    func pop() {
+        paths.removeLast()
+    }
+
+    func reset() {
+        paths.removeLast(paths.count)
+    }
+
+    func popToRoot() {
+        paths.removeAll()
+    }
+}
