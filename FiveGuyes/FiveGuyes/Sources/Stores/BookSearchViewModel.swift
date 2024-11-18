a32b025893a4717856c2835252246294f9eeb9a0
웰디 <hyunheeshim0103@gmail.com>
2024-11-18 17:14:01 +0900

    feat:#80-nilCheck_update
    feat:#80-nilCheck_update_forPush
    
    feat: #80 add 2s delay searching function
    
    feat: #80 confirm nil-logic, delete 2s delay searching function

--- FiveGuyes/FiveGuyes/Sources/Stores/BookSearchViewModel.swift
+++ FiveGuyes/FiveGuyes/Sources/Stores/BookSearchViewModel.swift
@@ -12,16 +12,21 @@ final class BookSearchViewModel: ObservableObject {
     
     @Published var books = [Book]()
     @Published var selectedBook: Book?
-    
+    @Published var isLoading = false
+    @Published var isDelaying: Bool = false
+    private var debounceWorkItem: DispatchWorkItem? // 딜레이를 위한 작업 아이템
+        
     private let apiStore = APIStore()
 
     func searchBooks(query: String) async {
+        self.isLoading = true
         do {
             let books = try await apiStore.fetchBooks(query: query)
             self.books = books
         } catch {
             print("Failed to fetch books: \(error)")
         }
+        self.isLoading = false
     }
 
     func fetchBookTotalPages(isbn: String) async -> String {
