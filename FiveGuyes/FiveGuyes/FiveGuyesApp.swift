3bbf7f63bde484b187a2c12350d2b5b073d535df
zaehorang <zaehorang2@naver.com>
2024-11-23 18:40:43 +0900

    feat: #71 Add App Tracking Transparency permission request

--- FiveGuyes/FiveGuyes/FiveGuyesApp.swift
+++ FiveGuyes/FiveGuyes/FiveGuyesApp.swift
@@ -5,6 +5,8 @@
 //  Created by zaehorang on 10/3/24.
 //
 
+import AdSupport
+import AppTrackingTransparency
 import SwiftData
 import SwiftUI
 
@@ -23,9 +25,31 @@ struct FiveGuyesApp: App {
     }
 }
 
+// MARK: - AppDelegate class
 class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-      FirebaseApp.configure()
-      return true
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // Firebase 초기화
+        FirebaseApp.configure()
+        
+        // 앱 추적 권한 요청을 비동기적으로 처리
+        Task {
+            await requestTrackingAuthorization()
+        }
+        return true
+    }
+    
+    /// 비동기 추적 권한 요청 함수
+    private func requestTrackingAuthorization() async {
+        // 0.5초 지연 후 추적 권한 요청
+        do {
+            try await Task.sleep(nanoseconds: 500_000_000) // 0.5초 = 500,000,000 나노초
+        } catch {
+            print(error)
+        }
+        
+        if ATTrackingManager.trackingAuthorizationStatus == .notDetermined {
+            let _ = await ATTrackingManager.requestTrackingAuthorization()
+        }
     }
 }
