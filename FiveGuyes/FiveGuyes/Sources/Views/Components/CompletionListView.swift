05dd367677e249c78330097653e22941ad5e85c7
zaehorang <zaehorang2@naver.com>
2024-11-26 16:40:24 +0900

    fix: #98 Resolve ForEach error caused by incorrect accessor connection
    - ForEach문에서 에러가 발생하던 문제를 해결하였습니다.
    - 문제는 ForEach문 자체가 아닌 내부 콘텐츠에서 접근 연산자가 올바르게 연결되지 않아 발생한 것이었습니다.
    - ForEach의 생산자가 콘텐츠 내부에서 잘못된 연산자로 인식되어 에러가 발생했습니다.
    - 관련 문서: [ForEach 초기화 메서드](https://developer.apple.com/documentation/swiftui/foreach/init(_:id:editactions:content:)).

--- FiveGuyes/FiveGuyes/Sources/Views/Components/CompletionListView.swift
+++ FiveGuyes/FiveGuyes/Sources/Views/Components/CompletionListView.swift
@@ -34,85 +34,84 @@ struct CompletionListView: View {
             }
             
             if !completedBooks.isEmpty {
-                EmptyView()
-//                VStack(alignment: .leading, spacing: 16) {
-//                    // 가로 스크롤로 completedBooks 보여주기
-//                    ScrollView(.horizontal, showsIndicators: false) {
-//                        HStack(spacing: 20) {
-//                            ForEach(completedBooks.indices, id: \.self) { index in
-//                                let book = completedBooks[index]
-//                                
-//                                VStack(alignment: .leading, spacing: 6) {
-//                                    if let coverURL = book.book.coverURL, let url = URL(string: coverURL) {
-//                                        AsyncImage(url: url) { image in
-//                                            image
-//                                                .resizable()
-//                                                .scaledToFit()
-//                                                .frame(width: 115, height: 178)
-//                                        } placeholder: {
-//                                            ProgressView()
-//                                        }
-//                                    } else {
-//                                        Image("bookCoverDummy")
-//                                            .resizable()
-//                                            .scaledToFit()
-//                                            .frame(width: 115, height: 178)
-//                                            .clipShape(RoundedRectangle(cornerRadius: 8))
-//                                    }
-//                                    
-//                                    VStack(alignment: .leading, spacing: 0) {
-//                                        Text(book.book.title )
-//                                            .font(.system(size: 14, weight: .semibold))
-//                                            .foregroundColor(.black)
-//                                        Text(book.book.author)
-//                                            .font(.system(size: 12, weight: .medium))
-//                                            .foregroundColor(Color(red: 0.44, green: 0.44, blue: 0.44))
-//                                    }
-//                                }
-//                                .frame(width: 115)
-//                                .onTapGesture {
-//                                    selectedBookIndex = index
-//                                }
-//                                .opacity(selectedBookIndex == index ? 1.0 : 0.3)
+                VStack(alignment: .leading, spacing: 16) {
+                    // 가로 스크롤로 completedBooks 보여주기
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 20) {
+                            ForEach(completedBooks.indices, id: \.self) { index in
+                                let book = completedBooks[index]
+                                
+                                VStack(alignment: .leading, spacing: 6) {
+                                    if let coverURL = book.bookMetaData.coverURL, let url = URL(string: coverURL) {
+                                        AsyncImage(url: url) { image in
+                                            image
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 115, height: 178)
+                                        } placeholder: {
+                                            ProgressView()
+                                        }
+                                    } else {
+                                        Image("bookCoverDummy")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 115, height: 178)
+                                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                                    }
+                                    
+                                    VStack(alignment: .leading, spacing: 0) {
+                                        Text(book.bookMetaData.title)
+                                            .font(.system(size: 14, weight: .semibold))
+                                            .foregroundColor(.black)
+                                        Text(book.bookMetaData.author)
+                                            .font(.system(size: 12, weight: .medium))
+                                            .foregroundColor(Color(red: 0.44, green: 0.44, blue: 0.44))
+                                    }
+                                }
+                                .frame(width: 115)
+                                .onTapGesture {
+                                    selectedBookIndex = index
+                                }
+                                .opacity(selectedBookIndex == index ? 1.0 : 0.3)
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    // 선택된 책의 소감문 및 기타 정보 표시
+                    let selectedBook = completedBooks[selectedBookIndex] 
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text(selectedBook.completionStatus.completionReview)
+                            .font(.system(size: 16, weight: .medium))
+                            .foregroundColor(.black)
+                            .padding(.bottom, 10)
+                        // TODO: 수정 버튼 추가하기
+                        HStack {
+                            Text("\(selectedBook.userSettings.targetEndDate.toKoreanDateStringWithoutYear()) 완독완료")
+                            Spacer()
+                            // TODO: ❗️❗️❗️ 수정하기 기능 추가
+                            // 데이터를 지우니까 튕김
+//                            Button {
+//                                showCompletionAlert = true
+//                            } label: {
+//                                Image(systemName: "ellipsis")
+//                                    .resizable()
+//                                    .scaledToFit()
+//                                    .frame(width: 20, height: 22)
+//                                    .tint(Color(red: 0.44, green: 0.44, blue: 0.44))
+//                                    .padding(.trailing, 3)
 //                            }
-//                        }
-//                    }
-//                    .padding(.horizontal, 20)
-//                    
-//                    // 선택된 책의 소감문 및 기타 정보 표시
-//                    let selectedBook = completedBooks[selectedBookIndex] 
-//                    VStack(alignment: .leading, spacing: 10) {
-//                        Text(selectedBook.completionReview)
-//                            .font(.system(size: 16, weight: .medium))
-//                            .foregroundColor(.black)
-//                            .padding(.bottom, 10)
-//                        // TODO: 수정 버튼 추가하기
-//                        HStack {
-//                            Text("\(selectedBook.book.targetEndDate.toKoreanDateStringWithoutYear()) 완독완료")
-//                            Spacer()
-//                            // TODO: ❗️❗️❗️ 수정하기 기능 추가
-//                            // 데이터를 지우니까 튕김
-////                            Button {
-////                                showCompletionAlert = true
-////                            } label: {
-////                                Image(systemName: "ellipsis")
-////                                    .resizable()
-////                                    .scaledToFit()
-////                                    .frame(width: 20, height: 22)
-////                                    .tint(Color(red: 0.44, green: 0.44, blue: 0.44))
-////                                    .padding(.trailing, 3)
-////                            }
-//                        }
-//                        .font(.system(size: 12, weight: .medium))
-//                        .foregroundColor(Color(red: 0.44, green: 0.44, blue: 0.44))
-//                    }
-//                    .padding(20)
-//                    .background {
-//                        RoundedRectangle(cornerRadius: 16)
-//                            .foregroundColor(Color(red: 0.95, green: 0.98, blue: 0.96))
-//                    }
-//                    
-//                }
+                        }
+                        .font(.system(size: 12, weight: .medium))
+                        .foregroundColor(Color(red: 0.44, green: 0.44, blue: 0.44))
+                    }
+                    .padding(20)
+                    .background {
+                        RoundedRectangle(cornerRadius: 16)
+                            .foregroundColor(Color(red: 0.95, green: 0.98, blue: 0.96))
+                    }
+                    
+                }
                 
             } else {
                 Rectangle()
