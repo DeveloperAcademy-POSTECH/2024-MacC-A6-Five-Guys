9dc60a53e3bc88280a96f564df3f8d6ba8cea66b
zaehorang <zaehorang2@naver.com>
2025-01-08 02:04:04 +0900

    feat: #156 Add UserBookDTO for decoupling data layer from views
    - UserBookDTO를 추가하여 데이터 레이어와 뷰를 분리
    - 기존 모델에 의존하지 않고 DTO를 통해 데이터를 전달할 수 있도록 개선
    - 향후 SwiftData와 서버 데이터 간 확장성을 고려한 설계 반영

---
+++ FiveGuyes/FiveGuyes/Sources/Models/UserBookDTO.swift
@@ -0,0 +1,73 @@
+//
+//  UserBookDTO.swift
+//  FiveGuyes
+//
+//  Created by zaehorang on 1/8/25.
+//
+
+import Foundation
+
+struct UserBookDTO: Identifiable {
+    struct BookMetaData {
+        let title: String
+        let author: String
+        let coverImageURL: String?
+        let totalPages: Int
+    }
+    
+    struct UserSettings {
+        let startPage: Int
+        let targetEndPage: Int
+        let initialPage: Date
+        let goalEndPage: Date
+        let excludedReadingDays: [Date]
+    }
+    
+    struct ReadingProgress {
+        let dailyReadingRecords: [String: ReadingRecord] // 날짜와 읽은 페이지 수의 매핑
+        let lastReadDate: Date?
+        let lastReadPage: Int
+    }
+    
+    struct CompletionStatus {
+        let isCompleted: Bool
+        let reviewAfterCompletion: String
+    }
+    
+    let id: UUID
+    let bookMetaData: BookMetaData
+    var userSettings: UserSettings
+    var readingProgress: ReadingProgress
+    var completionStatus: CompletionStatus
+}
+
+// Extension to convert UserBookV2 to UserBookDTO
+extension UserBookSchemaV2.UserBookV2 {
+    func toDTO() -> UserBookDTO {
+        return UserBookDTO(
+            id: self.id,
+            bookMetaData: UserBookDTO.BookMetaData(
+                title: self.bookMetaData.title,
+                author: self.bookMetaData.author,
+                coverImageURL: self.bookMetaData.coverURL,
+                totalPages: self.bookMetaData.totalPages
+            ),
+            userSettings: UserBookDTO.UserSettings(
+                startPage: self.userSettings.startPage,
+                targetEndPage: self.userSettings.targetEndPage,
+                initialPage: self.userSettings.startDate,
+                goalEndPage: self.userSettings.targetEndDate,
+                excludedReadingDays: self.userSettings.nonReadingDays
+            ),
+            readingProgress: UserBookDTO.ReadingProgress(
+                dailyReadingRecords: self.readingProgress.readingRecords,
+                lastReadDate: self.readingProgress.lastReadDate,
+                lastReadPage: self.readingProgress.lastPagesRead
+            ),
+            completionStatus: UserBookDTO.CompletionStatus(
+                isCompleted: self.completionStatus.isCompleted,
+                reviewAfterCompletion: self.completionStatus.completionReview
+            )
+        )
+    }
+}
